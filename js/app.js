30d4edf0d30aa8434791c675f9341a699a7c3564
zoozeezoo <zoozeezoo@gmail.com>
2016-11-22 22:10:52 +0100

    1 wersja

---
+++ js/app.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+            var boxTitle = document.querySelectorAll('.boxTitle')
+
+
+            for (var i = 0; i < boxTitle.length; i++) {
+
+                boxTitle[i].addEventListener('click', function() {
+                        var imgBoxText = this.nextElementSibling;
+                        if (imgBoxText.style.visibility = 'hidden') {
+
+                              imgBoxText.style.visibility = 'visible';
+                            } else {
+
+                              imgBoxText.style.visibility = 'hidden'
+                              // imgBoxText.addEventListener('click', function () {
+                              //   imgBoxText.style.visibility = 'initial';
+                              // })
+
+
+
+                              // imgBoxText.style.display = 'none';
+                            }
+
+                            // for (var i = 0; i < imgBoxText.length; i++) {
+                            //     imgBoxText.classList.toggle('showText');
+                            // };
+                        });
+                };
+            });
